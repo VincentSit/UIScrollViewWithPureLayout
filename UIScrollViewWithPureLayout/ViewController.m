91a5ba49cf215e1f69f1baecc050db6823657400
Vincent <vincentxueios@gmail.com>
2014-11-10 19:10:01 +0800

    first commit

--- UIScrollViewWithPureLayout/ViewController.m
+++ UIScrollViewWithPureLayout/ViewController.m
@@ -8,7 +8,21 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import <PureLayout.h>
+
+
+#define DEMO_1 1
+#define DEMO_2 0
+#define DEMO_3 0
+
+@interface ConentView : UIView
+@end
+
+@implementation ConentView
+
+- (CGSize)intrinsicContentSize {
+  return CGSizeMake(2000.f, 2000.f);
+}
 
 @end
 
@@ -16,12 +30,70 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  
+  NSParameterAssert(DEMO_1 + DEMO_2 + DEMO_3 <= 1);
+  
+#if DEMO_1
+  [self setupScrollView];
+#elif DEMO_2
+  [self setupScrollView2];
+#elif DEMO_3
+  [self setupScrollView3];
+#endif
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (void)setupScrollView {
+  UIScrollView *scrollView = [UIScrollView newAutoLayoutView];
+  scrollView.backgroundColor = [UIColor redColor];
+  [self.view addSubview:scrollView];
+  
+  UIImageView *contentView = [UIImageView newAutoLayoutView];
+  contentView.image = [UIImage imageNamed:@"BigImage"];
+  [scrollView addSubview:contentView];
+  [contentView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+  [scrollView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
 }
+
+- (void)setupScrollView2 {
+  UIScrollView *scrollView = [UIScrollView newAutoLayoutView];
+  scrollView.backgroundColor = [UIColor redColor];
+  [self.view addSubview:scrollView];
+  
+  ConentView *contentView = [ConentView newAutoLayoutView];
+  contentView.backgroundColor = [UIColor blueColor];
+  [scrollView addSubview:contentView];
+  
+  [contentView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+  [scrollView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+}
+
+- (void)setupScrollView3 {
+  UIScrollView *scrollView = [UIScrollView newAutoLayoutView];
+  scrollView.backgroundColor = [UIColor greenColor];
+  [self.view addSubview:scrollView];
+  
+  UIView *contentView = [UIView newAutoLayoutView];
+  contentView.backgroundColor = [UIColor orangeColor];
+  [scrollView addSubview:contentView];
+  
+  [contentView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+  [scrollView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+}
+
+#if DEMO_3
+- (void)viewDidLayoutSubviews {
+  [super viewDidLayoutSubviews];
+  
+  UIScrollView *scrollView = nil;
+  for (UIView *view in self.view.subviews) {
+    if ([view isKindOfClass:[UIScrollView class]]) {
+      scrollView = (UIScrollView *)view;
+      break;
+    }
+  }
+  
+  scrollView.contentSize = CGSizeMake(2000, 2000);
+}
+#endif
 
 @end
